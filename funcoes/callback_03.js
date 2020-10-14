ca6bf1de93ac20617f4c1e17eeee73330cd80b1f
Pablo <pablo.mg@hotmail.com>
2020-10-13 23:14:32 -0300

    added correction

--- funcoes/callback_03.js
+++ funcoes/callback_03.js
@@ -10,7 +10,7 @@ console.log(nome, letras)
 const carrinho = [
     { nome: 'Caneta', qtde: 10, preco: 7.99 },
     { nome: 'Impressora', qtde: 0, preco: 649.50 },
-    { nome: 'Caderno', qtde: 4, preco: 27.10 }
+    { nome: 'Caderno', qtde: 4, preco: 27.10 },
     { nome: 'Lapis', qtde: 3, preco: 5.82 },
     { nome: 'Tesoura', qtde: 1, preco: 19.20 },
 ]
