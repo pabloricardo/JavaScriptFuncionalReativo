6d8687a9a3d5a548763672bb1e1a7396a3d337c5
Pablo <pablo.mg@hotmail.com>
2020-10-18 11:58:23 -0300

    added example promise4

--- funcoes/promise_4.js
+++ funcoes/promise_4.js
@@ -1,14 +1,28 @@
 const { resolve } = require("path")
 
-function gerarNumerosEntre(min, max){
-    if(min > max){
+function gerarNumerosEntre(min, max, tempo) {
+    if (min > max) {
         [max, min] = [min, max]
     }
 
     return new Promise(resolve => {
-        const aleatorio = parseInt(Math.random() * (max - min + 1)) + min
-        resolve(aleatorio)
+        setTimeout(() => {
+            const aleatorio = parseInt(Math.random() * (max - min + 1)) + min
+            resolve(aleatorio)
+        }, tempo);
     })
 }
 
-gerarNumerosEntre(1,60).then(console.log)
+function gerarVariosNumeros() {
+    return Promise.all([
+        gerarNumerosEntre(1,60, 4000),
+        gerarNumerosEntre(1,60,1000),
+        gerarNumerosEntre(1,60,500),
+        gerarNumerosEntre(1,60,3000),
+        gerarNumerosEntre(1,60,100),
+        gerarNumerosEntre(1,60,1500),
+    ])
+}
+
+gerarVariosNumeros().then(console.log)
+
